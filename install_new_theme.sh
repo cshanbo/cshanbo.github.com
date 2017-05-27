e1174fd92194c0e373d9589866470027fd037efc
Shanbo Cheng <cshanbO@gmail.com>
2017-05-27 19:22:04 +0800

    add install new theme

---
+++ install_new_theme.sh
@@ -0,0 +1,20 @@
+if [ $# -ne 2 ]
+then
+    echo "usage: $0 + git-repository theme-name"
+    exit
+fi
+
+git clone $1 theme-cloned
+mkdir backup
+mv _config.yml _layouts _includes _sass assets backup/
+
+cd  theme-cloned
+cp -rf _layouts/ _includes _sass assets .. 
+cd ..
+rm -rf theme-cloned
+
+original=`grep "theme:" backup/_config.yml`
+
+cat ./backup/_config.yml | sed -e "s/${original}/theme: $2/g" > _config.yml.new
+mv _config.yml _config.yml.backup
+mv _config.yml.new _config.yml
